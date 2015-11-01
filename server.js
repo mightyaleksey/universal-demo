494b9fdb5a3cb7dbe6bc9cccff1f67b40a423fc2
Alexey Litvinov <iamhoneyboy@gmail.com>
2015-11-01 15:14:10 +0300

    css-modules-require-hook update

--- server.js
+++ server.js
@@ -1,25 +1,9 @@
 'use strict';
 
-var format = require('util').format;
-var interpolateName = require('loader-utils').interpolateName;
 var hook = require('css-modules-require-hook');
-var path = require('path');
-var loaderPath = path.relative(__dirname, require.resolve('postcss-loader/index.js'));
 
 hook({
-  generateScopedName: function (exportedName, exportedPath) {
-    var loaderContext = {resourcePath: exportedPath};
-    var name = '[name]__[local]___[hash:base64:5]'.replace('[local]', exportedName);
-    var options = {
-      content: format('%s "./%s!.%s"',
-        exportedName,
-        loaderPath,
-        exportedPath),
-      context: __dirname
-    };
-
-    return interpolateName(loaderContext, name, options);
-  }
+  generateScopedName: '[name]__[local]___[hash:base64:5]'
 });
 
 require('babel/register');
