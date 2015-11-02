deb8c42a21e26faa7b53aebc6d2af501fb27a9e9
Alexey Litvinov <iamhoneyboy@gmail.com>
2015-11-02 03:06:54 +0300

    hook version update

--- server.js
+++ server.js
@@ -7,19 +7,7 @@ var path = require('path');
 var loaderPath = path.relative(__dirname, require.resolve('postcss-loader/index.js'));
 
 hook({
-  generateScopedName: function (exportedName, exportedPath) {
-    var loaderContext = {resourcePath: exportedPath};
-    var name = '[name]__[local]___[hash:base64:5]'.replace('[local]', exportedName);
-    var options = {
-      content: format('%s "./%s!.%s"',
-        exportedName,
-        loaderPath,
-        exportedPath),
-      context: __dirname
-    };
-
-    return interpolateName(loaderContext, name, options);
-  }
+  generateScopedName: '[name]__[local]___[hash:base64:5]'
 });
 
 require('babel/register');
